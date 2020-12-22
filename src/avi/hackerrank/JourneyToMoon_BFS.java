e4767868cbcc039ecc65d8ae1bdfb9f4b9af1c6b
meavinash05 <meavinash05@gmail.com>
2020-12-22 11:30:53 +1100

    Implemented Union-Find for Kruskal MST and JourneyToMoon Problem

--- src/avi/hackerrank/JourneyToMoon_BFS.java
+++ src/avi/hackerrank/JourneyToMoon_BFS.java
@@ -7,20 +7,24 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 class JourneyToMoon {
-	
+
 	static int recursionCount = 0;
-	
 
 	static int fact(int n) {
-		if(n==1) return 1;
-		return n*fact(n-1);
+		if (n == 1)
+			return 1;
+		return n * fact(n - 1);
 	}
-	
+
 	public static long modifiedBFS(List<List<Integer>> adjList, boolean[] visited, int s) {
 		long sameCountryAstronauts = 1;
 		LinkedList<Integer> queue = new LinkedList<Integer>();
@@ -41,20 +45,37 @@ class JourneyToMoon {
 		return sameCountryAstronauts;
 	}
 
+	private static int parent(int currentElement, int parent[]) {
+		if (parent[currentElement] == currentElement)
+			return currentElement;
+		else {
+			int k = parent(parent[currentElement], parent);
+			parent[currentElement] = k;
+			return k;
+		}
+	}
+
+	private static void union(int start, int end, int parent[]) {
+		int parent_s = parent(start, parent), parent_e = parent(end, parent);
+		if (parent_s != parent_e) {
+			parent[parent_s] = parent_e;
+		}
+	}
+
 	/**
 	 * 
-     * For the unweighted graph, <name>:
-     *
-     * 1. The number of nodes is <name>Nodes.
-     * 2. The number of edges is <name>Edges.
-     * 3. An edge exists between <name>From[i] to <name>To[i].
-	 * @param graphTo 
-	 * @param graphFrom 
-	 * @param graphNodes 
-	 * @param y 
-	 * @param x 
-     *
-     *
+	 * For the unweighted graph, <name>:
+	 *
+	 * 1. The number of nodes is <name>Nodes. 2. The number of edges is <name>Edges.
+	 * 3. An edge exists between <name>From[i] to <name>To[i].
+	 * 
+	 * @param graphTo
+	 * @param graphFrom
+	 * @param graphNodes
+	 * @param y
+	 * @param x
+	 *
+	 *
 	 * 
 	 * @param graphNodes
 	 * @param graphFrom
@@ -64,6 +85,7 @@ class JourneyToMoon {
 	 * @return
 	 */
 	public static long solve(Integer graphNodes, List<Integer> graphFrom, List<Integer> graphTo) {
+
 		List<List<Integer>> adjList = new ArrayList<List<Integer>>(graphNodes);
 		for (int i = 0; i < graphNodes; i++) {
 			adjList.add(new ArrayList<Integer>());
@@ -72,51 +94,91 @@ class JourneyToMoon {
 			adjList.get(graphFrom.get(i)).add(graphTo.get(i));
 			adjList.get(graphTo.get(i)).add(graphFrom.get(i));
 		}
+
+		/*
+		 * The same problem can be solved by Union-Find for disjoint sets. Uncomment to
+		 * see the implementation
+		 */
+		//getGraphComponentsDetails_UnionFind(graphNodes, graphFrom, graphTo);
+
 		boolean[] visited = new boolean[graphNodes];
 		int graphComponents = 0;
-		long [] eachgraphComponentLength = new long[graphNodes];
-		for(int i=0;i<visited.length;i++) {
-			if(!visited[i]) {
-				eachgraphComponentLength[graphComponents]=JourneyToMoon.modifiedBFS(adjList, visited, i);
-				graphComponents=graphComponents+1;
+		long[] eachgraphComponentLength = new long[graphNodes];
+		for (int i = 0; i < visited.length; i++) {
+			if (!visited[i]) {
+				eachgraphComponentLength[graphComponents] = JourneyToMoon.modifiedBFS(adjList, visited, i);
+				graphComponents = graphComponents + 1;
 			}
 		}
-		long totalPairs = (1l*graphNodes*(graphNodes-1))/2;
+		long totalPairs = (1l * graphNodes * (graphNodes - 1)) / 2;
 		long sameCountryPairs = 0;
-		for(int i = 0;i<graphComponents;i++) {
+		for (int i = 0; i < graphComponents; i++) {
 			long l = eachgraphComponentLength[i];
-			sameCountryPairs = sameCountryPairs+(l*(l-1))/2;
+			sameCountryPairs = sameCountryPairs + (l * (l - 1)) / 2;
 		}
-		return (totalPairs-sameCountryPairs);
+		return (totalPairs - sameCountryPairs);
 	}
-	
-	
+
+	private static void getGraphComponentsDetails_UnionFind(Integer graphNodes, List<Integer> graphFrom,
+			List<Integer> graphTo) {
+		int parent[] = new int[graphNodes];
+		for (int i = 1; i < parent.length; i++) {
+			parent[i] = i;
+		}
+
+		for (int i = 0; i < graphFrom.size(); i++) {
+			JourneyToMoon.union(graphFrom.get(i), graphTo.get(i), parent);
+		}
+
+		boolean[] visitedNodes = new boolean[graphNodes];
+		Map<Integer, HashSet<Integer>> rootMapSets = new HashMap<Integer, HashSet<Integer>>();
+		for (int i = 0; i < parent.length; i++) {
+			if(!visitedNodes[i]) {
+			HashSet<Integer> set = new HashSet<Integer>();
+			int k = i;
+			while (parent[k] != k) {
+				set.add(k);
+				set.add(parent[k]);
+				visitedNodes[k]=true;
+				k = parent[k];
+			}
+			visitedNodes[k]=true;
+			if(null != rootMapSets.get(k)) {
+				HashSet<Integer> currentSet =  rootMapSets.get(k);
+				currentSet.addAll(set);	
+				rootMapSets.put(k, currentSet);
+			}else
+			rootMapSets.put(k, set);
+			}
+		}
+	}
+
 }
+
 public class JourneyToMoon_BFS {
 
 	public static void main(String[] args) throws IOException {
 		File file = new File("resources\\JourneyToMoon_BFS_Output.txt");
 		file.createNewFile();
-		BufferedReader bufferedReader = new BufferedReader(
-				(new FileReader("resources\\JourneyToMoon_BFS_Input.txt")));
+		BufferedReader bufferedReader = new BufferedReader((new FileReader("resources\\JourneyToMoon_BFS_Input.txt")));
 		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
 		int queries = Integer.parseInt(bufferedReader.readLine());
-		while(queries>0) {
-		String[] params = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
-		Integer graphNodes = Integer.parseInt(params[0]);
-		Integer graphEdges = Integer.parseInt(params[1]);
-		List<Integer> graphFrom = new ArrayList<>();
-		List<Integer> graphTo = new ArrayList<>();
-		  for (int i = 0; i < graphEdges; i++) {
-	            String[] astronautRowItems = bufferedReader.readLine().trim().split(" ");
-	                graphFrom.add(Integer.parseInt(astronautRowItems[0]));
-	                graphTo.add(Integer.parseInt(astronautRowItems[1]));
-	        }
-		long result = JourneyToMoon.solve(graphNodes, graphFrom, graphTo);
+		while (queries > 0) {
+			String[] params = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+			Integer graphNodes = Integer.parseInt(params[0]);
+			Integer graphEdges = Integer.parseInt(params[1]);
+			List<Integer> graphFrom = new ArrayList<>();
+			List<Integer> graphTo = new ArrayList<>();
+			for (int i = 0; i < graphEdges; i++) {
+				String[] astronautRowItems = bufferedReader.readLine().trim().split(" ");
+				graphFrom.add(Integer.parseInt(astronautRowItems[0]));
+				graphTo.add(Integer.parseInt(astronautRowItems[1]));
+			}
+			long result = JourneyToMoon.solve(graphNodes, graphFrom, graphTo);
 
-		bufferedWriter.write(String.valueOf(result));
-		bufferedWriter.newLine();
-		queries--;
+			bufferedWriter.write(String.valueOf(result));
+			bufferedWriter.newLine();
+			queries--;
 		}
 		bufferedReader.close();
 		bufferedWriter.close();
---
+++ src/avi/hackerrank/Kruskal_Graph_MST.java
@@ -0,0 +1,158 @@
+package avi.hackerrank;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+class Graph {
+
+	int numberOfVertex, numberOfEdge;
+	List<Edge> edges;
+
+	Graph(int numberOfVertex, int numberOfEdge) {
+		this.numberOfVertex = numberOfVertex;
+		this.numberOfEdge = numberOfEdge;
+		edges = new ArrayList<Edge>(numberOfEdge);
+		for (int i = 0; i < numberOfEdge; i++) {
+			edges.add(new Edge());
+		}
+	}
+
+	public List<List<Integer>> getAdjacencyList() {
+		List<List<Integer>> adjList = new ArrayList<List<Integer>>(numberOfVertex);
+		for (int i = 0; i < numberOfVertex; i++) {
+			adjList.add(new ArrayList<Integer>());
+		}
+		for (int i = 0; i < edges.size(); i++) {
+			adjList.get(edges.get(i).source).add(edges.get(i).destination);
+			adjList.get(edges.get(i).destination).add(edges.get(i).source);
+		}
+		return adjList;
+	}
+}
+
+class Edge implements Comparable<Edge> {
+	int source, destination, weight;
+
+	@Override
+	public int compareTo(Edge compareEdge) {
+		return this.weight - compareEdge.weight;
+	}
+
+	@Override
+	public String toString() {
+		return "Edge [source=" + source + ", destination=" + destination + ", weight=" + weight + "]";
+	}
+	
+}
+
+class subset {
+	int parent, rank;
+};
+
+class Kruskal_MST {
+
+	static int find(subset subsets[], int i) {
+		// find root and make root as parent of i
+		// (path compression)
+		if (subsets[i].parent != i)
+			subsets[i].parent = find(subsets, subsets[i].parent);
+
+		return subsets[i].parent;
+	}
+
+	static void Union(subset subsets[], int x, int y) {
+		int xroot = find(subsets, x);
+		int yroot = find(subsets, y);
+
+		// Attach smaller rank tree under root
+		// of high rank tree (Union by Rank)
+		if (subsets[xroot].rank < subsets[yroot].rank)
+			subsets[xroot].parent = yroot;
+		else if (subsets[xroot].rank > subsets[yroot].rank)
+			subsets[yroot].parent = xroot;
+
+		// If ranks are same, then make one as
+		// root and increment its rank by one
+		else {
+			subsets[yroot].parent = xroot;
+			subsets[xroot].rank++;
+		}
+	}
+
+	public static List<Edge> solve(Graph graph) {
+		List<Edge> result = new ArrayList<Edge>(graph.numberOfVertex);
+		for (int i = 0; i < graph.numberOfVertex; ++i)
+			result.add(new Edge());
+		Collections.sort(graph.edges);
+		subset subsets[] = new subset[graph.numberOfVertex];
+		for (int i = 0; i < graph.numberOfVertex; ++i)
+			subsets[i] = new subset();
+
+		for (int v = 0; v < graph.numberOfVertex; ++v) {
+			subsets[v].parent = v;
+			subsets[v].rank = 0;
+		}
+		int i = 0, e = 0; // Index used to pick next edge
+
+		// Number of edges to be taken is equal to V-1
+		while (e < graph.numberOfVertex - 1) {
+			// Step 2: Pick the smallest edge. And increment
+			// the index for next iteration
+			Edge next_edge = new Edge();
+			next_edge = graph.edges.get(i);
+			i++;
+
+			int x = find(subsets, next_edge.source);
+			int y = find(subsets, next_edge.destination);
+
+			// If including this edge does't cause cycle,
+			// include it in result and increment the index
+			// of result for next edge
+			if (x != y) {
+				result.set(e, next_edge);
+				e++;
+				Union(subsets, x, y);
+			}
+			// Else discard the next_edge
+		}
+		return result;
+	}
+}
+
+public class Kruskal_Graph_MST {
+
+	public static void main(String[] args) throws IOException {
+		File file = new File("resources\\Kruskal_Graph_MST_Output.txt");
+		file.createNewFile();
+		BufferedReader bufferedReader = new BufferedReader((new FileReader("resources\\Kruskal_Graph_MST_Input.txt")));
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file));
+		int queries = Integer.parseInt(bufferedReader.readLine());
+		while (queries > 0) {
+			String[] params = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+			Integer graphNodes = Integer.parseInt(params[0]);
+			Integer graphEdges = Integer.parseInt(params[1]);
+			Graph graph = new Graph(graphNodes, graphEdges);
+			for (int i = 0; i < graphEdges; i++) {
+				String[] graphEdge = bufferedReader.readLine().trim().split(" ");
+				graph.edges.get(i).source = Integer.parseInt(graphEdge[0]);
+				graph.edges.get(i).destination = Integer.parseInt(graphEdge[1]);
+				if (null != graphEdge[2])
+					graph.edges.get(i).weight = Integer.parseInt(graphEdge[2]);
+			}
+			List<Edge> results = Kruskal_MST.solve(graph);
+			bufferedWriter.write(results.toString());
+			bufferedWriter.newLine();
+			queries--;
+		}
+		bufferedReader.close();
+		bufferedWriter.close();
+	}
+}
